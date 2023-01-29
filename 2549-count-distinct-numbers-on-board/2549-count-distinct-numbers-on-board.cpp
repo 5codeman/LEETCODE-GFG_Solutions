91e9bad8f88e75fde5944e776b12f4ade99a9123
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-01-30 00:41:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (64.29%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    // queue/ stack se bhi bun shakta hai 
+     
+    //ye q discuss se samghe hai
+    int distinctIntegers(int n) 
+    {
+        if(n == 1) return 1;
+        return n-1;
+    }
+};
