f4cbf471a7fa1e55077a7438568fbcab61451a83
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-02-13 20:03:07 +0530

    Added solution - LeetHub

---
+++ Arithmetic Number - GFG/arithmetic-number.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int a, int b, int c){
+        // code here
+        if(c == 0)
+        {
+            if(a == b) return 1;
+            else return 0;
+        }
+        else if(c > 0)
+        {
+            if(a > b) return 0;
+            else if((b-a)%c == 0) return 1;
+            else return 0;
+        }
+        else
+        {
+            if(a < b) return 0;
+            else if((b-a)%c == 0) return 1;
+            else return 0;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
