3033e3c8dc44c61d64faca90e24a078158baf95b
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-04-27 10:26:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // Fantastic q but take help from discuss section
+        // iss q ko baad mai aache se karna
+        
+        return sqrt(n);
+
+    }
+};
