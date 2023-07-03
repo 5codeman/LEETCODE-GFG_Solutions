4f0d4693354c6b7d8a37d8f8e721d9084ff18f0b
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-07-04 02:56:08 +0530

    Time: 854 ms (30.54%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,22 @@
+/* get all the even numbered rows as odd numbered rows */
+SELECT s1.id - 1 as id, s1.student
+FROM Seat s1
+WHERE s1.id MOD 2 = 0
+
+UNION
+
+/* get all the odd numbered rows as even numbered rows */
+SELECT s2.id + 1 as id, s2.student
+FROM Seat s2
+WHERE s2.id MOD 2 = 1 AND s2.id != (SELECT MAX(id) FROM Seat)
+/* Just don't get the last row as we will handle it in the next UNION */
+
+UNION
+
+/* get the last row if odd and don't change the id value */
+SELECT s3.id, s3.student
+FROM Seat s3
+WHERE s3.id MOD 2 = 1 AND s3.id = (SELECT MAX(id) FROM Seat)
+
+/* Order the result by id */
+ORDER BY id ASC;
