c69f6fd05ffb6af4f27e1cf13b86434efaa1604f
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-07-04 14:49:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.72%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        // khud bhi aata tha but normal approach se ye wala discuss se sikhe
-        long x = n ^ (n >> 1); 
-        x = x & (x+1);
+        long x = n ^ (n>>1);
+        long y = x & (x+1);
         
-        return x==0;
+        if(y == 0) return true;
+        
+        return false;
     }
 };
