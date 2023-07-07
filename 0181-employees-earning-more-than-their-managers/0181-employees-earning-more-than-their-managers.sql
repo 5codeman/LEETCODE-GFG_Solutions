91340c4733178f916bfd5646a7393adabe111a68
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-07-07 23:26:55 +0530

    Time: 854 ms (47.46%), Space: 0B (100.00%) - LeetHub

---
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select
+   e1.name as Employee 
+from
+   employee e1 
+   join
+      employee e2 
+where
+   e1.managerid = e2.id 
+   and e1.salary > e2.salary;
