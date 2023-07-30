ab9749951c7bf3c473f569dfb09cc6fffe5ba3da
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-07-31 01:01:43 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (41.76%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -14,31 +14,32 @@ public:
             
             else
             {
-                string temp = "";
-                int nums = 0;
-                
+                // Find the string inside the bracket
+                string temp = "";                
                 while(st.top() != "[")
                 {
                     temp = st.top() + temp;
                     st.pop();
                 }
-                st.pop();
-
+                st.pop(); // pop the "[" opening bracket
+                
+                // Finding the numeric part
                 string num = "";
                 while(!st.empty() && st.top() >= "0" && st.top() <= "9")
                 {
                     num = st.top() + num;
                     st.pop();
                 }
-                int integer = stoi(num);
+                int integer = stoi(num); // change string to integer
                 
+                // Expanding the temp string integer times
                 string temp1 = "";
                 for(int j = 0; j < integer; j++)
                 {
                     temp1 += temp;
                 }
                                 
-                st.push(temp1);
+                st.push(temp1); // push the string in stack
             }
         }
         
