f1eeec975e16207fdb95cb5b53f32556f4d928ba
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-07-31 23:34:45 +0530

    Time: 27 ms (97.51%), Space: 24 MB (47.85%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> arr(len,-1);
         stack<int>st;
         
-        for(int i = 0; i < (2*len)-1; i++)
+        for(int i = 0; i < (2*len); i++)
         {
             if(!st.empty() && nums[i % len] > nums[st.top()])
             {
