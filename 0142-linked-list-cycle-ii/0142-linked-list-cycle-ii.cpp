6f1d8a0055976b57ac78f20e1b12292c64f6dfad
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-08-07 13:54:12 +0530

    Time: 11 ms (42.51%), Space: 10.1 MB (9.56%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,20 +6,21 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// we can also do by o(n) (but uska proove samghna aache se fir karna, sont solve without watching video)
+
+// and try to solve by my approach find k length and then think how solve
 #include<unordered_map>
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*, bool>map;
+        unordered_map<ListNode*, bool> map;
         ListNode *temp = head;
         while(temp != NULL)
         {
-            if(map.count(temp) == 1)
-            {
-                return temp;
-            }
+            if(map.count(temp) == 1) return temp;
             map[temp] = true;
-            temp = temp->next;
+            temp = temp -> next;
         }
         return NULL;
     }
