c724b8a814c111461136d060876535792503b3a0
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-09-07 02:25:16 +0530

    Time: 7 ms (18.65%), Space: 9.5 MB (70.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //Find the correct row for target element using Binary Search
+        int start = 0;
+        int end = matrix.size() - 1;
+        int target_row = -1;
         
-        //iss q ko baad mai binary search aur bhiu aahe approach se karna......
-        int row=matrix.size();
-        int col=matrix[0].size();
-        for(int i=0;i<row;i++)
+        while(start <= end)
         {
-            for(int j=0;j<col;j++)
+            int mid = (start + end) / 2;
+            int col = matrix[mid].size() - 1;
+            if(matrix[mid][0] <= target && matrix[mid][col] >= target)  
             {
-                if(matrix[i][j]==target) return true;
+                target_row = mid;
+                break;
+            }
+            
+            else if(matrix[mid][col] > target) end = mid - 1;
+            
+            else start = mid + 1;
+        }
+        if(target_row != -1)
+        {
+            // Binary search for finding target in the target row
+            int start = 0;
+            int end = matrix[target_row].size() - 1;
+            while(start <= end)
+            {
+                int mid = (start + end) / 2;
+                if(matrix[target_row][mid] == target) return true;
+            
+                else if(matrix[target_row][mid] > target) end = mid - 1;
+            
+                else start = mid + 1;
             }
         }
         return false;
