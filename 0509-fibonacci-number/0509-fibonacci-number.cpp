b9e1cec42c5eb7e1e2ee935bbea2f7cd2c43ed80
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-11-22 01:42:57 +0530

    Time: 13 ms (26.08%), Space: 6.2 MB (98.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0||n==1)
-        {
-            return n;
-        }
+        if(n == 0 || n == 1) return n;
         
-        int small_output=fib(n-1);
-        int small_output1=fib(n-2);
-        
-        int nth_term=small_output+small_output1;
-        
-        return nth_term;
+        return fib(n - 1) + fib(n - 2);
     }
 };
